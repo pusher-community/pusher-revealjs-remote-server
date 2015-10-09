5b3087cb4d06abfd7f9cb30889545d4502841b4f
leggetter <phil@leggetter.co.uk>
2015-10-09 13:25:55 +0100

    first attempt at open server

---
+++ index.js
@@ -0,0 +1,30 @@
+var Pusher = require('pusher');
+var express = require('express');
+var bodyParser = require('body-parser');
+
+// TODO: allow config to be defined in a config.json for more flexability
+
+var config = {
+  appId: process.env.PUSHER_APP_ID,
+  key: process.env.PUSHER_APP_KEY,
+  secret: process.env.PUSHER_APP_SECRET,
+};
+
+var pusher = new Pusher(config);
+
+var app = express();
+app.use(bodyParser.json());
+
+app.get('/pusher-auth', function (req, res) {
+  var socketId = req.body.socket_id;
+  var channelName = req.body.channel_name;
+  
+  var auth = pusher.authenticate(socketId, channelName);
+  res.send(auth);
+});
+
+var server = app.listen((process.env.PORT || 5000), function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
+});
